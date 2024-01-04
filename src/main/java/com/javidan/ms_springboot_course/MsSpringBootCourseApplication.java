9e3b6cde6ce885d571836a63edb122b169b47189
JavidanH <jahajiyev@gmail.com>
2024-01-04 22:08:22 +0400

    task5 Make simple REST api, which you will use pathvariable, requestParam, RequestHeader

---
+++ src/main/java/com/javidan/ms_springboot_course/MsSpringBootCourseApplication.java
@@ -0,0 +1,13 @@
+package com.javidan.ms_springboot_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringBootCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSpringBootCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javidan/ms_springboot_course/MsSpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javidan.ms_springboot_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSpringBootCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
